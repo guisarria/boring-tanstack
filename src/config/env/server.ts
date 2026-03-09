01417a1766d5a4ca1db95532962f017a247e1a06
Guilherme Sarria <guisarria@gmail.com>
2026-03-09 16:04:47 -0300

    fix: remove nitro

--- src/config/env/server.ts
+++ src/config/env/server.ts
@@ -5,7 +5,6 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z.url(),
     BETTER_AUTH_SECRET: z.string(),
-    BETTER_AUTH_URL: z.url(),
     GITHUB_CLIENT_ID: z.string().optional(),
     GITHUB_CLIENT_SECRET: z.string().optional(),
     GOOGLE_CLIENT_ID: z.string().optional(),
