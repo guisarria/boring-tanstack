dc6c2b0364330e3ad0794f158c7941e34376e9de
Guilherme Sarria <guisarria@gmail.com>
2026-03-09 14:14:15 -0300

    feat: cloudflare

--- src/config/env/client.ts
+++ src/config/env/client.ts
@@ -1,7 +1,10 @@
 import { createEnv } from "@t3-oss/env-core"
+import z from "zod"
 
 export const env = createEnv({
   clientPrefix: "VITE_",
-  client: {},
+  client: {
+    VITE_BASE_URL: z.url(),
+  },
   runtimeEnv: import.meta.env,
 })
--- src/modules/auth/auth-client.ts
+++ src/modules/auth/auth-client.ts
@@ -1,9 +1,9 @@
 import { inferAdditionalFields } from "better-auth/client/plugins"
 import { createAuthClient } from "better-auth/react"
-import { env } from "@/config/env/server"
+import { env } from "@/config/env/client"
 import type { auth } from "./auth"
 
 export const authClient = createAuthClient({
-  baseURL: env.BETTER_AUTH_URL,
+  baseURL: env.VITE_BASE_URL,
   plugins: [inferAdditionalFields<typeof auth>()],
 })
