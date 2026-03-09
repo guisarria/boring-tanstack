0c3aff3c9338a5c655c3e470be9ec1139ba5cc07
Guilherme Sarria <guisarria@gmail.com>
2026-03-09 16:25:23 -0300

    fix: remove nitro

--- src/modules/auth/auth.ts
+++ src/modules/auth/auth.ts
@@ -1,15 +1,15 @@
 import { betterAuth } from "better-auth"
 import { drizzleAdapter } from "better-auth/adapters/drizzle"
 import { tanstackStartCookies } from "better-auth/tanstack-start"
-import { env } from "@/config/env/client"
+import { env } from "@/config/env/server"
 import { db } from "@/db"
 import * as schema from "./schema"
 
 export const auth = betterAuth({
   appName: "Boring Template",
-  baseURL: env.VITE_BASE_URL,
+  baseURL: env.BASE_URL,
   experimental: { joins: true },
-  trustedOrigins: [env.VITE_BASE_URL],
+  trustedOrigins: [env.BASE_URL],
   database: drizzleAdapter(db, {
     provider: "pg",
     usePlural: true,
