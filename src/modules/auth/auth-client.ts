4918b3cf7e06e2c528110c353b110ed879bb537d
Guilherme Sarria <guisarria@gmail.com>
2026-03-09 14:09:29 -0300

    feat: cloudflare

--- src/modules/auth/auth-client.ts
+++ src/modules/auth/auth-client.ts
@@ -1,9 +1,9 @@
 import { inferAdditionalFields } from "better-auth/client/plugins"
 import { createAuthClient } from "better-auth/react"
-import { env } from "@/config/env/client"
+import { env } from "@/config/env/server"
 import type { auth } from "./auth"
 
 export const authClient = createAuthClient({
-  baseURL: env.VITE_BETTER_AUTH_URL,
+  baseURL: env.BETTER_AUTH_URL,
   plugins: [inferAdditionalFields<typeof auth>()],
 })
