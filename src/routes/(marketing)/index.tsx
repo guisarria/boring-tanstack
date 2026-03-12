920fd4233cbfdffe1a5d4dd9ff7c026152316a54
Guilherme Sarria <94687153+guisarria@users.noreply.github.com>
2026-03-12 12:35:32 -0300

    fix: form

--- src/routes/(marketing)/index.tsx
+++ src/routes/(marketing)/index.tsx
@@ -1,12 +1,10 @@
 import { createFileRoute } from "@tanstack/react-router"
-import { Container, Main, Section } from "@/components/ui/design-system"
+import { Main } from "@/components/ui/design-system"
 import { Hero } from "@/routes/(marketing)/-components/hero"
 import { StackSection } from "@/routes/(marketing)/-components/stack-section"
 import { BlurOverlay } from "./-components/blur-overlay"
 import { CloneRepository } from "./-components/clone-repository-section"
 import { Footer } from "./-components/footer"
-
-import { DashboardMockup } from "./-components/dashboard-mockup"
 
 export const Route = createFileRoute("/(marketing)/")({ component: Index })
 
@@ -15,11 +13,6 @@ function Index() {
     <>
       <Main className="flex flex-col items-center gap-y-20">
         <Hero />
-        <Section>
-          <Container>
-            <DashboardMockup />
-          </Container>
-        </Section>
         <StackSection />
         <CloneRepository />
       </Main>
